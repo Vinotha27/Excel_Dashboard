aedf6a55912c6d9e8774e4f39211932ecb8a1840
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-21 22:28:00 +0530

    Update Sales_data.py

--- Sales_data.py
+++ Sales_data.py
@@ -8,14 +8,7 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 # ---- READ EXCEL ----
 @st.cache
 def get_data_from_excel():
-    df = pd.read_excel(
-        io="supermarkt_sales.xlsx",
-        engine="openpyxl",
-        sheet_name="Sales",
-        skiprows=3,
-        usecols="B:R",
-        nrows=1000,
-    )
+    df = pd.read_excel("C:\Users\Guest-User\Downloads\supermarkt_sales.xlsx)
     # Add 'hour' column to dataframe
     df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
     return df
